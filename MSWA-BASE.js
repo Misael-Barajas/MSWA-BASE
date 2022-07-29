97a19093bf4ac12b2d33dfe00f5f9f88371d8daa
Misael <110029376+Misael-Mods@users.noreply.github.com>
2022-07-29 15:29:59 -0500

    Update MSWA-BASE.js

--- MSWA-BASE.js
+++ MSWA-BASE.js
@@ -1,1 +1,18 @@
-a
+{
+	"version": "2.0",
+	"download": "https://t.me/MsWhatsApp",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/Misael-Mods/MSWA-BASE/main/changelog-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/Misael-Mods/MSWA-BASE/main/changelog-es.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/Misael-Mods/MSWA-BASE/main/changelog-pt.html"
+		}
+	]
+}
